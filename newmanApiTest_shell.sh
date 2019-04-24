d531a4058745f55d264200eb1eb1a38129f425b1
xiaolanyun <2407512504@qq.com>
2019-04-24 17:34:45 +0800

    更新脚本

--- newmanApiTest_shell.sh
+++ newmanApiTest_shell.sh
@@ -2,7 +2,7 @@
 source /etc/profile
 cnpm install
 echo 正在进行接口自动化........
-newman run phonecheck.json -r html
+newman run phonecheck.json -reporterscli,html,json --reporter-json-export poptestjsonOut.json --reporter-html-export poptesthtmlOut.html
 echo 报告已经生成，正在打包......
 tar -zcvf ${BUILD_NUMBER}_${JOB_NAME}_report.tar.gz ./newman
 echo 请您查看邮件或者jenkins构建日志输出台！
