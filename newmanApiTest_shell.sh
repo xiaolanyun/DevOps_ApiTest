6fa969e1814e4d7b9c1c81c77cb8fadd14ebda9e
xiaolanyun <2407512504@qq.com>
2019-04-24 17:10:31 +0800

    shell脚本增加生成html报告

--- newmanApiTest_shell.sh
+++ newmanApiTest_shell.sh
@@ -2,5 +2,5 @@
 source /etc/profile
 cnpm install
 echo 正在进行接口自动化........
-newman run phonecheck.json –reporterscli,html,json,junit --reporter-json-export poptestjsonOut.json --reporter-junit-export poptestxmlOut.xml --reporter-html-export poptesthtmlOut.html
+newman run phonecheck.json -r html
 echo 报告已经生成，请查看日志！
